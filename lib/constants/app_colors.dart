6e5a3b4fe12399f94f17a84479ee95e918223466
sagadinao-wq <sagadinao@gmail.com>
2026-09-24 23:19:45 +0300

    Define app colors and exercise image assets
    Added color constants and exercise image mappings.

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+const kBgColor = Color(0xFF0A0A0A);
+const kCardColor = Color(0xFF1C1C1E);
+const kCard2Color = Color(0xFF232325);
+const kSubColor = Color(0xFF8E8E93);
+const kRedColor = Color(0xFFFF3B4E);
+const kGreenColor = Color(0xFF32D74B);
+const kDividerColor = Color(0xFF2C2C2E);
+const kCyanColor = Color(0xFF00C2FF);
+const kOrangeColor = Color(0xFFFF3A00);
+
+const Map<String, String> kExerciseImages = {
+  'Жим': 'assets/exercises/bench.jpg',
+  'Присід': 'assets/exercises/squat.jpg',
+  'Станова': 'assets/exercises/deadlift.jpg',
+};
